2d43c862e43c9dcdf495f0eb0e11fa8ac47dadc7
hcf0018 <hcf0018@unt.edu>
2020-05-02 15:31:48 -0500

    commenting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
-const url = 'mongodb://localhost:27017'; //
+const url = 'mongodb://localhost:27017'; //how to connect to locally hosted mongodb
 
 const dbName = 'spark'
 let db
