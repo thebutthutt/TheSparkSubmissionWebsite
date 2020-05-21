1adac42a401f8056e945d68e5e88bc0289855e8f
hcf0018 <hcf0018@libwebfactory.library.unt.edu>
2020-05-21 18:00:54 -0500

    cleanup

--- config/payment.js
+++ config/payment.js
@@ -2,7 +2,6 @@ const secret_key = "9683c083-8e24-44f5-8e5e-4e69186bc015";
 const base_url = "https://payments.library.unt.edu/payment/";
 const account = "spark-testing";
 
-var md5 = require('md5');
 var crypto = require('crypto');
 
 module.exports = {
@@ -10,8 +9,9 @@ module.exports = {
         var concatString = "";
         var newURL = new URL(base_url);
         concatString = concatString.concat(account, amount, contact_name, submissionID, secret_key);
-        console.log("concatstring", concatString);
+
         var otherHash = crypto.createHash('md5').update(concatString).digest("hex");
+
         newURL.searchParams.append("account", account);
         newURL.searchParams.append("amount", amount);
         newURL.searchParams.append("contact_name", contact_name);
@@ -32,17 +32,13 @@ module.exports = {
         var searchParams = new URLSearchParams(urlToTest);
 
         //concatenate all the params
-        concatString.concat(searchParams.get("amount"));
-        concatString.concat(searchParams.get("account"));
-        concatString.concat(searchParams.get("contact_name"));
-        concatString.concat(searchParams.get("submissionID"));
-        concatString.concat(secret_key);
+        concatString = concatString.concat(searchParams.get("amount"), searchParams.get("account"), searchParams.get("contact_name"), searchParams.get("submissionID"), secret_key);
 
         //hash the params
-        var hash = md5(concatString);
+        var otherHash = crypto.createHash('md5').update(concatString).digest("hex");
 
         //does is match the hash sent over?
-        if (hash == searchParams.get("libhash")) {
+        if (otherHash == searchParams.get("libhash")) {
             console.log("hashes match");
         } else {
             console.log("hashes dont match");
