b475ff3c8a80692bc9f1a5ec80802ed42fb034a8
hcf0018 <hcf0018@libwebfactory.library.unt.edu>
2020-05-29 15:58:14 -0500

    working on other requests

--- app/cleHandler.js
+++ app/cleHandler.js
@@ -1,7 +1,6 @@
 const formidable = require('formidable');
 const moment = require('moment');
 const constants = require('../config/constants');
-var payment = require('../config/payment.js');
 var fs = require('fs');
 var cleRequestModel = require('./models/cleRequest');
 
@@ -9,29 +8,24 @@ module.exports = {
     handleSubmission: function (req) {
         var time = moment(),
             unique = 1,
-            numFiles,
             filenames = [],
             additional = [];
         new formidable.IncomingForm().parse(req, function (err, fields, files) {
-            additional.push(time, numFiles, filenames);
-            if (fields.requestType == "cnc") {
-                module.exports.addCNC(fields, additional);
-            }
+            additional.push(time.format(constants.format), filenames);
+            module.exports.addEntry(fields, additional);
         }).on('fileBegin', (name, file) => { //when a new file comes through
             file.name = time.unix() + unique + "%$%$%" + file.name; //add special separater so we can get just the filename later
             //yes this is a dumb way to keep track of the original filename but I dont care
             unique += 1; //increment unique so every file is not the same name
-            file.path = __dirname + '/uploads/' + file.name;
+            file.path = __dirname + '/uploads/clefiles/' + file.name;
         }).on('file', function (name, file) {
             filenames.push(file.path);
-            numFiles++;
         });
-
     },
 
-    addCNC: function (fields, additional) {
+    addEntry: function (fields, additional) {
         var request = new cleRequestModel(); //new instance of a request
-        request.type = "CNC";
+        request.type = fields.requestType;
         request.patron = {
             fname: fields.first,
             lname: fields.last,
@@ -39,14 +33,18 @@ module.exports = {
             euid: fields.euid,
         }
         request.notes = fields.notes;
+        request.submissionDate = additional[0];
+        request.files = additional[1];
+        request.maker = "Unassigned";
+        request.completed = false;
+        request.completedDate = "Uncompleted";
 
-    },
+        request.save(function (err, document) {
+            if (err) {
+                return console.error(err);
+            }
+        });
 
-    addLaser: function () {
-
-    },
-
-    addEmbroidery: function () {
-
+        console.log(request);
     }
 }
---
+++ public/js/workrequests.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.remove-btn').on('click', function () {
+        var submissionID = $(this).attr('id');
+        $.ajax({
+            method: "POST",
+            url: "/workrequests/delete",
+            data: {
+                "submissionID": submissionID
+            },
+            dataType: "json"
+        }).done(function () {
+            location.reload();
+        });
+    });
+
+    $('.download-btn').on('click', function () {
+        var fileID = $(this).attr('id');
+        window.location = '/workrequests/download?fileID=' + fileID;
+    });
+
+    $('.claim-btn').on('click', function () {
+        let submissionID = $(this).attr('id');
+        $.ajax({
+            type: 'POST',
+            url: '/workrequests/claim',
+            data: {
+                "submissionID": submissionID
+            },
+            dataType: 'json'
+        }).done(function () {
+            location.reload();
+        });
+    });
+
+    $('.delete-file-btn').on('click', function () {
+        let fileID = $(this).attr('id');
+        $.ajax({
+            type: 'POST',
+            url: '/workrequests/deletefile',
+            data: {
+                "fileID": fileID
+            },
+            dataType: 'json'
+        }).done(function () {
+            location.reload();
+        });
+    });
+});
