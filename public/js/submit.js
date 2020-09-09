69cce5515af8010accc1ead254502b2aa2334f83
hcf0018 <hcf0018@libwebfactory.library.unt.edu>
2020-09-09 15:48:34 -0500

    fix case sensitivity when uploading stls

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,100 +1,101 @@
 var addOne = function () {
-  $.ajax({
-    type: "GET",
-    url: "/oneprint",
-    dataType: "html",
-    success: function (data) {
-      $("#files-list").append(data);
-      $(".custom-file-input").on("change", function () {
-        if (
-          $(this)
-            .val()
-            .substring($(this).val().length - 4) != ".stl"
-        ) {
-          $(this).attr("type", "text");
-          $(this).attr("type", "file");
-          $(this).popover({
-            trigger: "focus",
-          });
-          $(this).popover("show");
-        } else {
-          $(this).popover("hide");
-          let fileName = $(this).val().split("\\").pop();
-          $(this)
-            .siblings(".custom-file-label")
-            .addClass("selected")
-            .html(fileName);
-        }
-      });
-    },
-  });
+    $.ajax({
+        type: "GET",
+        url: "/oneprint",
+        dataType: "html",
+        success: function (data) {
+            $("#files-list").append(data);
+            $(".custom-file-input").on("change", function () {
+                if (
+                    $(this)
+                        .val()
+                        .substring($(this).val().length - 4)
+                        .toUpperCase() != ".STL"
+                ) {
+                    $(this).attr("type", "text");
+                    $(this).attr("type", "file");
+                    $(this).popover({
+                        trigger: "focus",
+                    });
+                    $(this).popover("show");
+                } else {
+                    $(this).popover("hide");
+                    let fileName = $(this).val().split("\\").pop();
+                    $(this)
+                        .siblings(".custom-file-label")
+                        .addClass("selected")
+                        .html(fileName);
+                }
+            });
+        },
+    });
 };
 
 var addOneOther = function () {
-  $.ajax({
-    type: "GET",
-    url: "/onefile",
-    dataType: "html",
-    success: function (data) {
-      $("#files-list-other").append(data);
-      $(".custom-file-input").on("change", function () {
-        let fileName = $(this).val().split("\\").pop();
-        $(this)
-          .siblings(".custom-file-label")
-          .addClass("selected")
-          .html(fileName);
-      });
-    },
-  });
+    $.ajax({
+        type: "GET",
+        url: "/onefile",
+        dataType: "html",
+        success: function (data) {
+            $("#files-list-other").append(data);
+            $(".custom-file-input").on("change", function () {
+                let fileName = $(this).val().split("\\").pop();
+                $(this)
+                    .siblings(".custom-file-label")
+                    .addClass("selected")
+                    .html(fileName);
+            });
+        },
+    });
 };
 
 $(document).ready(function () {
-  var numPrints = 1;
-  var requestType = "print";
-  //this inserts the first 3d print file segment
-  $("#submission-form").attr("action", "/submitprint");
-  $("#files-list-other").empty();
-  $(".non-print").hide();
-  $(".print-only").show();
-  addOne();
-  $(".print-only input").prop("required", true);
+    var numPrints = 1;
+    var requestType = "print";
+    //this inserts the first 3d print file segment
+    $("#submission-form").attr("action", "/submitprint");
+    $("#files-list-other").empty();
+    $(".non-print").hide();
+    $(".print-only").show();
+    addOne();
+    $(".print-only input").prop("required", true);
 
-  //this hides the 3d print specific section of the form when the dropdown isnt 3d print
-  $("#requestTypeSelector").change(function () {
-    requestType = $(this).children("option:selected").val();
-    if (requestType == "print") {
-      $("#submission-form").attr("action", "/submitprint");
-      $("#files-list-other").empty();
-      $(".non-print").hide();
-      $(".print-only").show();
-      addOne();
-      $(".print-only input").prop("required", true);
-    } else {
-      $("#submission-form").attr("action", "/submitcle");
-      $("#files-list").empty();
-      $(".print-only").hide();
-      $(".non-print").show();
-      $(".print-only input").prop("required", false);
-    }
-  });
+    //this hides the 3d print specific section of the form when the dropdown isnt 3d print
+    $("#requestTypeSelector").change(function () {
+        requestType = $(this).children("option:selected").val();
+        if (requestType == "print") {
+            $("#submission-form").attr("action", "/submitprint");
+            $("#files-list-other").empty();
+            $(".non-print").hide();
+            $(".print-only").show();
+            addOne();
+            $(".print-only input").prop("required", true);
+        } else {
+            $("#submission-form").attr("action", "/submitcle");
+            $("#files-list").empty();
+            $(".print-only").hide();
+            $(".non-print").show();
+            $(".print-only input").prop("required", false);
+        }
+    });
 
-  //adds new 3d print section when add another button is clicked
-  $("#change").click(addOne);
+    //adds new 3d print section when add another button is clicked
+    $("#change").click(addOne);
 
-  //adds new file section when add another button is clicked
-  $("#change-other").click(addOneOther);
+    //adds new file section when add another button is clicked
+    $("#change-other").click(addOneOther);
 
-  //removes the last 3d print file segment when the remove one button is clicked
-  $("#remove-other").click(function () {
-    $(".single-file:last").remove();
-  });
+    //removes the last 3d print file segment when the remove one button is clicked
+    $("#remove-other").click(function () {
+        $(".single-file:last").remove();
+    });
 
-  //removes the last 3d print file segment when the remove one button is clicked
-  $("#remove").click(function () {
-    $(".single-print:last").remove();
-  });
+    //removes the last 3d print file segment when the remove one button is clicked
+    $("#remove").click(function () {
+        $(".single-print:last").remove();
+    });
 });
 
 $(document).on("submit", "#submission-form", function () {
-  $(".modal").modal("show");
+    $(".modal").modal("show");
 });
