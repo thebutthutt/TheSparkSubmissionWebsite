06ad7b298c5b11538c866d592f8bbbf12aeeec93
hcf0018 <hcf0018@libwebfactory.library.unt.edu>
2020-09-23 09:19:36 -0500

    fix deployment issues

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 cd $(dirname $0)
-/usr/bin/node --tls-cipher-list='ECDHE-RSA-AES256-SHA384:AES256-SHA256:!RC4:HIGH:!MD5:!aNULL:!EDH:!EXP:!SSLV2:!eNULL' /home/hcf0018/webserver/TheSparkSubmissionWebsite/server.js
+if [[ $(sudo netstat -tulpn | grep :27017) ]]; then
+    echo "mongod running, starting server"
+    /usr/bin/node --tls-cipher-list='ECDHE-RSA-AES256-SHA384:AES256-SHA256:!RC4:HIGH:!MD5:!aNULL:!EDH:!EXP:!SSLV2:!eNULL' /home/hcf0018/webserver/TheSparkSubmissionWebsite/server.js
+else
+    echo "mongod not running, starting mongod and server"
+    sudo mongod & /usr/bin/node --tls-cipher-list='ECDHE-RSA-AES256-SHA384:AES256-SHA256:!RC4:HIGH:!MD5:!aNULL:!EDH:!EXP:!SSLV2:!eNULL' /home/hcf0018/webserver/TheSparkSubmissionWebsite/server.js
+fi
