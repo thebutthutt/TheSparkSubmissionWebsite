03932724b7b65658913bfe2c60f10f282570d5b4
Hanna <pancakebatter12345@gmail.com>
2021-01-11 14:11:21 -0600

    change logs location

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    apps : [{
-        name: "sparkorders",
-        script: "/home/hcf0018/webserver/TheSparkSubmissionWebsite/deploy.sh",
-        watch: true,
-        error_file: '/home/hcf0018/webserver/logs/err.log',
-        out_file: '/home/hcf0018/webserver/logs/out.log',
-        log_file: '/home/hcf0018/webserver/logs/combined.log',
-        time: true
-    }]
-  }
-  
+    apps: [
+        {
+            name: "sparkorders",
+            script: "/home/hcf0018/webserver/TheSparkSubmissionWebsite/deploy.sh",
+            watch: true,
+            error_file: "/home/hcf0018/webserver/NewLogs/err.log",
+            out_file: "/home/hcf0018/webserver/NewLogs/out.log",
+            log_file: "/home/hcf0018/webserver/NewLogs/combined.log",
+            time: true,
+        },
+    ],
+};
