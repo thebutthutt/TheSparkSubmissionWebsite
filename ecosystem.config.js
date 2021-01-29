f74a393736df9ffc01e4247d9e86a8c96ed13fc0
Hanna <pancakebatter12345@gmail.com>
2021-01-29 13:31:42 -0600

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,8 @@ module.exports = {
     apps: [
         {
             name: "sparkorders",
-            script: "/home/hcf0018/webserver/TheSparkSubmissionWebsite/deploy.sh",
-            watch: true,
-            error_file: "/home/hcf0018/webserver/NewLogs/err.log",
-            out_file: "/home/hcf0018/webserver/NewLogs/out.log",
-            log_file: "/home/hcf0018/webserver/NewLogs/combined.log",
-            time: true,
-        },
-        {
-            name: "nodespark",
-            script: "/home/hcf0018/webserver/TheSparkSubmissionWebsite/server.js",
-            args:
-                "--tls-cipher-list='ECDHE-RSA-AES256-SHA384:AES256-SHA256:!RC4:HIGH:!MD5:!aNULL:!EDH:!EXP:!SSLV2:!eNULL'",
+            script: "npm",
+            args: "start",
             watch: true,
             error_file: "/home/hcf0018/webserver/NewLogs/err.log",
             out_file: "/home/hcf0018/webserver/NewLogs/out.log",
